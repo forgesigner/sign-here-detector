846ec7796b02ea1b88caa604b5e117061b1f8ec0
khram2003 <nikita.khramov@gmail.com>
2023-12-05 14:26:30 +0100

    onnx checkpoint

--- train.py
+++ train.py
@@ -91,6 +91,8 @@ def train_center_net(
         if avg_val_loss < best_val_loss:
             best_val_loss = avg_val_loss
             torch.save(checkpoint, os.path.join(checkpoint_path, "best_checkpoint.pth"))
+            dummy_input = torch.randn(1, 3, 936, 662, device=device)
+            torch.onnx.export(model, dummy_input, os.path.join(checkpoint_path, "best_checkpoint.onnx"))
 
 
 def main():
